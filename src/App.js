0677a5b19ef152f9c4b4313896f5354dcd835b21
phyolwinlwinlatt <myathsumon554@gmail.com>
2021-07-28 08:07:22 +0630

    contact-form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import './scss/App.scss';
+import ContactFormPage from './pages/ContactFormPage';
+import FormDataPreviewPage from './pages/FormDataPreviewPage';
+import SuccessPage from './pages/SuccessPage';
+import { Route } from 'react-router-dom';
+import AboutusPage from './pages/AboutusPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path='/' exact component={ContactFormPage} />
+      <Route path='/preview' exact component={FormDataPreviewPage} />
+      <Route path='/success' exact component={SuccessPage} />
+      <Route path='/aboutus' exact component={AboutusPage} />
     </div>
   );
 }
